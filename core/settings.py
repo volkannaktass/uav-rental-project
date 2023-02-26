01d35615099ee4b6d1f121e178753720eeb494d8
volkannaktass <volkanaktas98@gmail.com>
2023-02-26 18:30:06 +0300

    created dockerfile and compose

--- core/settings.py
+++ core/settings.py
@@ -83,15 +83,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"), 
-        'USER': env("DB_USER"), 
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
